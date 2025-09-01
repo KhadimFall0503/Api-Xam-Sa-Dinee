4165a0b036f35ec6540e18e85d9ad98e1e49d6cf
Khadim Fall <khadimfall0503@gmail.com>
2025-09-01 10:16:03 +0000

    Authentification, Filtrage et REcherche

--- api/views.py
+++ api/views.py
@@ -1,7 +1,12 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from .models import Rubrique
 from .serializer import RubriqueSerializer
 
 class RubriqueViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Rubrique.objects.all()
     serializer_class = RubriqueSerializer
+    #permission_classes = [IsAuthenticated]  permet d'authentifier l'utilisateur avant d'accéder aux données 
+   #Filtrage et recherche
+    filrerset_fields = ['title']
+    search_fields = ['title', 'content']
--- server/settings.py
+++ server/settings.py
@@ -30,7 +30,16 @@ INSTALLED_APPS = [
     'api',
     'rest_framework',
     'corsheaders',
+    'django_filters',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend'
+    ]
+}
+
+
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',

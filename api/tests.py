83bf69998d2303b3236bd7619909ae8fd59e925d
Khadim Fall <khadimfall0503@gmail.com>
2025-10-07 13:46:03 +0000

    Test mon API

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+# api/tests.py
+from rest_framework.test import APITestCase
+from rest_framework import status
+from .models import Rubrique
 
-# Create your tests here.
+class TestRubriqueAPI(APITestCase):
+    def setUp(self):
+        self.rubrique = Rubrique.objects.create(title="Tech")
+
+    def test_get_rubriques(self):
+        response = self.client.get("/api/rubriques/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
